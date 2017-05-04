9aaf40d830f79982c26c01640ccab88b4b8d8b32
Alex <alexanderlperez@gmail.com>
2017-05-04 14:03:18 -0500

    refactored extraction to strategy pattern

--- index.php
+++ index.php
@@ -20,25 +20,55 @@ $token = getenv('ACCESS_TOKEN');
 *  Bot init  *
 **************/
 
-class MessageHandler {
-    protected $message;
+interface IMessageExtractionStrategy {
+    public function extract();
+}
+
+class UserMessageExtractor implements IMessageExtractionStrategy {
+    private $message;
 
     public function __construct($message) {
         $this->message = $message;
     }
 
-    public function respondWithDialogTreeByMessage() {
-        error_log('user message: ' . $this->message->message->text);
+    public function extract() {
+        return $this->message->message->text;
+    }
+}
+
+class PostbackExtractor implements IMessageExtractionStrategy {
+    private $message;
+
+    public function __construct($message) {
+        $this->message = $message;
     }
 
-    public function handlePostback() {
-        error_log('postback: ' . $this->message->postback->payload);
-        //var_error_log($this->message);
+    public function extract() {
+        return $this->message->postback->payload;
+    }
+}
+
+class LocationExtractor implements IMessageExtractionStrategy {
+    private $message;
+
+    public function __construct($message) {
+        $this->message = $message;
     }
 
-    public function handleLocation() {
-        error_log('location: ');
-        var_error_log($this->message->message->attachments[0]->payload->coordinates);
+    public function extract() {
+        return $this->message->message->attachments[0]->payload->coordinates;
+    }
+}
+
+class MessageHandler {
+    private $extractionStrategy;
+
+    public function __construct(IMessageExtractionStrategy $extractionStrategy) {
+        $this->extractionStrategy = $extractionStrategy;
+    }
+
+    public function handle() {
+        return $this->extractionStrategy->extract();
     }
 }
 
@@ -58,9 +88,6 @@ if ($isTokenValidation) {
 $data = json_decode(file_get_contents("php://input"), false); // return obj
 $hasMessage = !empty($data->entry[0]->messaging);
 
-//error_log('got response:');
-//var_error_log($data->entry[0]->messaging);
-
 if ($hasMessage) {
     foreach ($data->entry[0]->messaging as $message) {
         $isUserMessage = !empty($message->message) 
@@ -77,16 +104,20 @@ if ($hasMessage) {
         // - Location - Quick Reply: string
         // - Bar selection - List Template: string
 
-        $handler = new MessageHandler($message);
-
         if ($isUserMessage) {
-            $handler->respondWithDialogTreeByMessage();
+            $strategy = new UserMessageExtractor($message);
         } else if ($isPostback) {
-            $handler->handlePostback();
+            $strategy = new PostbackExtractor($message);
         } else if ($isLocation) {
-            $handler->handleLocation();
+            $strategy = new LocationExtractor($message);
         }
+
+        if (!isset($strategy)) {
+            die('Unkown message type');
+        }
+
+        $handler = new MessageHandler($strategy);
+        var_error_log($handler->handle());
     }
 }
 
-error_log('----------');
