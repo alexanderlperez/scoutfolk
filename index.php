cb2a96543ec476eee525f68accddbb3fa9d925a5
Alex <alexanderlperez@gmail.com>
2017-05-04 15:20:16 -0500

    can handle first dialog

--- index.php
+++ index.php
@@ -8,10 +8,15 @@
 require_once(dirname(__FILE__) . '/vendor/autoload.php');
 
 use Symfony\Component\Dotenv\Dotenv;
+
 use ScoutFolk\Strategies\UserMessageExtractor;
 use ScoutFolk\Strategies\PostbackExtractor;
 use ScoutFolk\Strategies\LocationExtractor;
-use ScoutFolk\Controllers\MessageHandler;
+
+use pimax\FbBotApp;
+use pimax\Messages\Message;
+use pimax\Messages\QuickReply;
+use pimax\Messages\QuickReplyButton;
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/.env');
@@ -22,6 +27,8 @@ $token = getenv('ACCESS_TOKEN');
 /**************
 *  Bot init  *
 **************/
+
+$bot = new FbBotApp($token);
 
 // Handle token verification
 
@@ -34,7 +41,36 @@ if ($isTokenValidation) {
     die($_REQUEST['hub_challenge']);
 }
 
-// Handle actual messages
+/***************************
+*  Decision Tree-related  *
+***************************/
+
+function makeDecisionTreeDialogResponse($bot, $text, $userId) {
+    if ($text == 'hi') {
+        startDialog($bot, $userId);
+    } else {
+        errorDialog($bot, $text, $userId);
+    }
+}
+
+function startDialog($bot, $userId) {
+    $bot->send(new Message($userId, "Hi! We're going to figure out how far you need to jog to get to the AnalogFolk HQ, and how many Clif Bars you'll need to eat.  Let's start shall we..."));
+    $bot->send(new QuickReply($userId, 'For starters, where are you?', [
+        new QuickReplyButton(QuickReplyButton::TYPE_LOCATION),
+    ]));
+}
+
+function errorDialog($bot, $text, $userId) {
+    $bot->send(new Message($userId, "Hmmm, I'm not sure I get \"" . $text . "\""));
+}
+
+function makeMapRenderingResponse() {
+    
+}
+
+/*************************
+*  Handle the messages  *
+*************************/
 
 $data = json_decode(file_get_contents("php://input"), false); // return obj
 $hasMessage = !empty($data->entry[0]->messaging);
@@ -51,22 +87,30 @@ if ($hasMessage) {
         $isPostback = !empty($message->postback);
 
         // Handle various "command payloads": user text, button text
-        // - Initial user text
-        // - Location - Quick Reply: string
-        // - Bar selection - List Template: string
 
         if ($isUserMessage) {
             $strategy = new UserMessageExtractor($message);
-        } else if ($isPostback) {
-            $strategy = new PostbackExtractor($message);
+            makeDecisionTreeDialogResponse($bot, $strategy->extract(), $message->sender->id);
         } else if ($isLocation) {
             $strategy = new LocationExtractor($message);
-        }
+            makeMapRenderingResponse($strategy->extract(), $message->sender->id);
+        } else if ($isPostback) {
+            $strategy = new PostbackExtractor($message);
+            //TODO: is this even used?
+        } 
 
         if (!isset($strategy)) {
             error_log('Unkown message type');
             die('Unkown message type');
         }
+
+        // TODO: handle the user appropriately
+        // - simple dialog tree: 
+        //   . inital user text -> start mapping
+        //   . any user text while mapping -> respond with 
+        //     * after bar choice -> "I don't know, but how about that favorite bar, huh?"
+        //     * after location -> "I don't know, but I could figure out how far you need to jog"
+        //     * after final rendering -> "Whew, I don't know, I'm a bit tired from all that calculating..."
 
         var_error_log($strategy->extract());
     }
